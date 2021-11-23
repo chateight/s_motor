58ff54d46824a406586433c7a87acd2f52dac64b
ryuichi <rusami.usual@icloud.com>
2021-11-23 20:49:26 +0900

    # # full function #

--- motor_drv.py
+++ motor_drv.py
@@ -30,9 +30,9 @@ class motor_drive:
 # GPIO17,GPIO22,GPIO23,GPIO24
   StepPins = [17,22,23,24]
 # shutdown and rotate direction pins
-  SHUTDOWN = 17   # need to be changed to other pins
-  r_left = 22     #
-  r_right = 20    #
+  SHUTDOWN = 16   
+  r_left = 25
+  r_right = 26
 
       
   def __init__(self):
@@ -46,32 +46,38 @@ class motor_drive:
           GPIO.setup(pin,GPIO.OUT)
           GPIO.output(pin, False)
       # shutdown & direction DIO definition
-
+      GPIO.setup(self.SHUTDOWN, GPIO.IN, GPIO.PUD_UP) 
       GPIO.setup(self.r_left, GPIO.IN, GPIO.PUD_UP) 
       GPIO.setup(self.r_right, GPIO.IN, GPIO.PUD_UP) 
 
-      # direction switch detection
-      GPIO.add_event_detect(self.r_left, GPIO.FALLING, bouncetime=100)
-      GPIO.add_event_detect(self.r_right, GPIO.FALLING, bouncetime=100)
-      # callback routine definition
-      GPIO.add_event_callback(self.r_left, self.sw_callback_left) 
-      GPIO.add_event_callback(self.r_right, self.sw_callback_right)
-
-      # shutdown wait and handle
+      def sw_callback(self):
+          subprocess.call('sudo shutdown -h now', shell=True)
+      #
+      # detect shutdown sw push
       GPIO.add_event_detect(self.SHUTDOWN, GPIO.FALLING,bouncetime=250)
-      # when the sw was pushed, call the 'callback routine' 
-      GPIO.add_event_callback(self.SHUTDOWN, self.sw_callback) 
+      # when the sw was pushed, call the 'call back routine' 
+      GPIO.add_event_callback(self.SHUTDOWN, sw_callback) 
 
-  def sw_callback(self):
-      subprocess.call('sudo shutdown -h now', shell=True)
-    #        
-  def sw_callback_left(self):
-      print('Callback left')
+      # direction switch detection
+      GPIO.add_event_detect(self.r_left, GPIO.FALLING, bouncetime=300)
+      GPIO.add_event_detect(self.r_right, GPIO.FALLING, bouncetime=300)
+      # callback routine definition
+      GPIO.add_event_callback(self.r_left, self.dir_sw_callback)
+      GPIO.add_event_callback(self.r_right, self.dir_sw_callback)
+
+  def dir_sw_callback(self, i_pin):
+      if i_pin == self.r_left:
+        while GPIO.input(self.r_left) == 0:
+          if GPIO.input(self.r_right) != 0:
+            self.rotate(-1)   # cw
+            time.sleep(0.03)
+      else:
+        while GPIO.input(self.r_right) == 0:
+          if GPIO.input(self.r_left) != 0:
+            self.rotate(1)    # ccw        
+            time.sleep(0.03) 
     
-  def sw_callback_right(self):
-      print('Callback right')
-
-  def rotate(self):
+  def rotate(self, r_dir):
 # Define advanced sequence
 # as shown in manufacturers datasheet
       Seq = [[1,0,0,1],
@@ -84,8 +90,8 @@ class motor_drive:
              [0,0,0,1]]
        
       StepCount = len(Seq)
-      StepDir = 1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
+      StepDir = r_dir # Set to 1 or 2 for clockwise
+                  # Set to -1 or -2 for anti-clockwise
 
 # Wait time
       WaitTime = 10/float(1000)
@@ -94,34 +100,35 @@ class motor_drive:
       StepCounter = 0
 
 # Start main lcls
-      while True:
-#  print(StepCounter,)
-#  print(Seq[StepCounter])
+      for count in range(64):
         for pin in range(0, 4):
           xpin = self.StepPins[pin]
-        if Seq[StepCounter][pin]!=0:
-            print(" Enable GPIO %i" %(xpin))
+          if Seq[StepCounter][pin]!=0:
+#            print(" Enable GPIO %i" %(xpin))
             GPIO.output(xpin, True)
-        else:
+          else:
             GPIO.output(xpin, False)
 
         StepCounter += StepDir
 
-  # If we reach the end of the sequence
-  # start again
+# If we reach the end of the sequence
+# start again
         if (StepCounter>=StepCount):
             StepCounter = 0
         if (StepCounter<0):
             StepCounter = StepCount+StepDir
 
-  # Wait before moving on
+# Wait before moving on
         time.sleep(WaitTime)
+# motor power off
+      for pin in self.StepPins:
+        GPIO.output(pin, False)
 
   def callback(self):
       while True:
           time.sleep(1)
 #
-# to make class instance
+# to make "motor_drive" class instance
 #
 md = motor_drive()
 # wait for sw falling edge
